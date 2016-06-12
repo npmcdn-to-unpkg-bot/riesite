fffe2d2b435d30e572df28cd750f4b01f041d813
Rany Azevedo <azevedo.rany@gmail.com>
2016-06-12 15:47:42 +0400

    add trigger event

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,72 +1,65 @@
+// Animate loader off screen
+$(window).load(function() {
+  $(".imageLoad").fadeOut("slow");
+  $(".handle").trigger("mousedown");
+
+});
+
+
 $(document).ready(function() {
+
+// 1. Animations in Social Media Links
   var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
   $('.social-media').on('mouseover', function() {
     $(this).addClass('animated rotateIn').one(animationEnd, function() {
       $(this).removeClass('animated ' + 'rotateIn');
     });
   });
-});
 
-// if($('body.pages').length === 0){
-//   return;
-// }
-
-
-
-$(document).ready(function() {
+// 2. Swal for like/dislike actions
   $('.like').on('click', function() {
-    swal({title: "You Love It",
-         text: "Now it's in your WishList",
-         timer: 90000,
-        showConfirmButton: true});
+    swal({
+      title: "You Love It",
+      text: "Now it's in your WishList",
+      timer: 90000,
+      showConfirmButton: true
     });
   });
 
-$(document).ready(function() {
   $('.dislike').on('click', function() {
-    swal({title: "You remove it from your wish list",
-         text: "Click again to add it",
-         timer: 90000,
-        showConfirmButton: true});
+    swal({
+      title: "You remove it from your wish list",
+      text: "Click again to add it",
+      timer: 90000,
+      showConfirmButton: true
+    });
   });
-});
 
-//background-image
-$(document).ready(function() {
 
-var leftWidth = $(".left").width();
-var bannerClicked = false;
+// 3. Re-size Background-image
+  var leftWidth = $(".left").width();
+  var bannerClicked = false;
 
-$(".handle").on("mousedown", function () {
-  if (bannerClicked) {
+  $(".handle").on("mousedown", function() {
+    if (bannerClicked) {
+      bannerClicked = false;
+    }
+    bannerClicked = true;
+  });
+
+  $(document).on("mouseup", function() {
     bannerClicked = false;
-  }
-  bannerClicked = true;
-});
+  });
 
-$(document).on("mouseup", function () {
-  bannerClicked = false;
-});
-
-$(".banner").on("mousemove", function (e) {
-  if (!bannerClicked) { return; }
-  $(".handle").css("left", e.screenX);
-  $(".left").width( e.screenX );
-});
+  $(".banner").on("mousemove", function(e) {
+    if (!bannerClicked) {
+      return;
+    }
+    $(".handle").css("left", e.screenX - 70);
+    $(".left").width(e.screenX);
+  });
 
 
 
-// $( window ).load(function() {
-//   $(window).resize(function() {
-//       if($(window).width() > 1023) {
-//         skrollr.init({
-//           forceHeight: false,
-//           smoothScrolling: false
-//             });
-//       } else {
-//
-//       }
-//     }).resize();
-// });
 
 });

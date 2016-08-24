e4e9ec395aabd0032849746ac7e47921b58c8ee0
Rany Azevedo <azevedo.rany@gmail.com>
2016-08-24 14:29:23 +0400

    payment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622144104) do
+ActiveRecord::Schema.define(version: 20160824101914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 20160622144104) do
     t.float    "total_revenue"
     t.text     "delivery_address"
     t.string   "shipping_id"
+    t.string   "wish_status"
   end
 
   add_foreign_key "favorites", "users"
